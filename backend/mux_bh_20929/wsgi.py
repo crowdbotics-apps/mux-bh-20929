fa163f8fa5f4cb706b5a8fa3d3f5f7f78a8cfc57
Crowdbotics <team@crowdbotics.com>
2020-10-01 13:35:08 +0000

    Update models

--- backend/mux_bh_20929/wsgi.py
+++ backend/mux_bh_20929/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mux_bh_20929.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mux_bh_20929.settings")
 
 application = get_wsgi_application()
